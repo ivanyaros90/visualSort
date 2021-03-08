66852f99b9c15f2343e7dc7365868da3ffcea361
Ivan Yaroslavskyy <ivanyaros90@gmail.com>
2021-03-08 16:33:49 +0100

    changes

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 var state={
   numbers:[""],
   doms:[""],
+  original:[],
   history:[],
   indexes:[],
   pivot:0,
@@ -8,23 +9,118 @@ var state={
   tableWidth:400,
   tableHeigth:200,
   elementWidth:20,
+  run: async function(){
+    let elem1=this.doms[0];
+    let elem2=this.doms[19];
+    elem1.style.transition="left 2s";
+    elem2.style.transition="left 2s";
+    elem1.style.left="380px";
+    elem2.style.left="0px";
+
+  },
+  calculateValues:function(ind,original){
+    let res=[];
+    ind.forEach(element => {
+      res.push(original[element]);     
+    });
+    console.log(res);
+    return res;
+  },
+  showButtons:function(){
+    let parent=document.getElementById("steps");
+    removeAllChildNodes(parent);
+    let i=0;
+    this.indexes.forEach(step => {
+      let s=document.createElement("li");
+      s.id="step"+i;
+      s.innerText="Step " +i++;
+      s.onclick=function(){
+        
+        let id=parseInt(this.id.substr(4));
+        window.state.step(id);
+        
+
+      };
+      parent.appendChild(s);
+
+      
+    });
+
+
+  },
+  renderStatic:function(values){
+    let parent= document.getElementById("idParent");
+    removeAllChildNodes(parent);
+    let i=0;
+    values.forEach(element=>{
+      [,y]=createValue(i++,this.elementWidth,element);
+      parent.appendChild(y);
+    })
+
+  },
+  step:function(id=0){
+    if(typeof this.indexes[id] === 'undefined') {
+      alert("step no existe"+id);
+    }else{
+      let now= this.indexes[id].slice();
+      let values=this.calculateValues(now,this.original);
+      this.renderStatic(values);
+
+
+
+    }
+  }, 
   generate: function(){
-  let parent= document.getElementById("idParent");
-  removeAllChildNodes(parent);
-  this.numbers=[];
-  this.doms=[];
-  this.elementWidth=20;
-  let actual=[];
-  for(let i=0;i<this.elements;i++){
-    let x,y;    
-    [x,y]=createValue(i,this.elementWidth);    
-    parent.appendChild(y);
-    this.numbers.push(x);
-    this.doms.push(y);
-    actual.push(i);
+    let parent= document.getElementById("idParent");
+    removeAllChildNodes(parent);
+    this.numbers=[];
+    this.doms=[];
+    this.elementWidth=20;
+    let actual=[];
+    this.indexes=[];
+    for(let i=0;i<this.elements;i++){
+      let x,y;    
+      [x,y]=createValue(i,this.elementWidth);    
+      parent.appendChild(y);
+      this.numbers.push(x);
+      this.doms.push(y);
+      actual.push(i);
+    }
+    this.original=this.numbers.slice();
+    this.indexes.push(actual); 
+  },
+  selectionSort: function(){
+    let len=this.numbers.length;  
+    for(let i=0;i<len-1;i++){
+      min_index=i;
+      for(let j=i+1;j<len;j++){
+        if(this.numbers[j]<this.numbers[min_index]) min_index=j;
+      }
+      this.selectionSwap(i,min_index,this.elementWidth);
+      let index=this.indexes[this.indexes.length-1].slice();
+      let t=index[i];
+      index[i]=index[min_index];
+      index[min_index]=t;
+      this.indexes.push(index);
+      this.history.push(this.numbers);
+      
+      
+    }
+
+  },
+  selectionSwap:function(i,j,width){
+    let temp= this.numbers[i];
+    this.numbers[i]=this.numbers[j];
+    this.numbers[j]=temp;
+    this.doms[i].style.left=(j*width)+"px";
+    this.doms[j].style.left=(i*width)+"px";
+    this.doms[j].style.backgroundColor="green";
+    temp=this.doms[i];
+    this.doms[i]=this.doms[j];
+    this.doms[j]=temp;
+    let index=this.indexes[this.indexes.length-1];    
+    
   }
-  this.indexes.push(actual); 
-}
 }
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
@@ -41,8 +137,11 @@ function removeAllChildNodes(parent){
      parent.removeChild(parent.firstChild);
   }
 };
-function createValue(pos,width){
-   let number= 50+Math.floor(Math.random()*151);
+function createValue(pos,width,val=""){
+   let number=val;
+   if(val===""){
+   number= 50+Math.floor(Math.random()*151);
+   }
    let valor=document.createElement("div");
    //valor.innerText=number;
    valor.style.width=width+"px";
@@ -56,27 +155,16 @@ function createValue(pos,width){
    valor.style.position="absolute";
    return [number,valor];
 }
-function generate(){
-  let parent= document.getElementById("idParent");
-  removeAllChildNodes(parent);
-  state.numbers=[];
-  state.doms=[];
-  for(let i=0;i<6;i++){
-    let x,y;    
-    [x,y]=createValue(i);    
-    parent.appendChild(y);
-    state.numbers.push(x);
-    state.doms.push(y);
-  }  
-}
+
 function buttonSelect(){
-  selectionSort(window.state,0,20);
+  window.state.selectionSort();
 }
 function buttonSelect2(){
   bubbleSort(window.state);
 }
 function show(){
-  console.log(window.state.history);
+  console.log(window.state.indexes);
+  window.state.showButtons();
 }
 async function mergeSort(state,i,j){
   if((j-i)>1){
@@ -165,7 +253,7 @@ async function bubbleSort(state){
   }while(repeat);
   
 }
-async function selectionSort(state){
+function selectionSort(state){
   let len=state.numbers.length;
   
   for(let i=0;i<len-1;i++){
@@ -174,6 +262,11 @@ async function selectionSort(state){
       if(state.numbers[j]<state.numbers[min_index]) min_index=j;
     }
     state=selectionSwap(state,i,min_index,state.elementWidth);
+    let index=state.indexes[state.indexes.length-1].slice();
+    let t=index[i];
+    index[i]=index[min_index];
+    index[min_index]=t;
+    state.indexes.push(index);
     state.history.push(state.numbers);
     //await sleep(1000);
     
